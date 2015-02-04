bb6dc7c5f377bc7d29f71bdf38f2e3b3448ce638
reka prihatanto <rey.dbarrolz@gmail.com>
2015-02-05 05:43:18 +0700

    tambah file migration data_user

---
+++ database/migrations/2015_02_04_223017_create_MstDataUser_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMstDataUserTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('mst_data_user', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('nama', 200);
+			$table->string('alamat');
+			$table->date('tgl_lahir');
+			$table->enum('jenis_kelamin', ['L', 'P']);
+			$table->string('no_hp', 20);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('mst_data_user');
+	}
+
+}
