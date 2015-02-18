e8a3b4934d4958aa5aa5884b3f7848c9aa4c0043
reka prihatanto <rey.dbarrolz@gmail.com>
2015-02-18 17:25:43 +0700

    tambah fitur kirim backup db ke email

--- app/Console/Commands/BackupDbToEmail.php
+++ app/Console/Commands/BackupDbToEmail.php
@@ -3,7 +3,8 @@
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
-
+use Ifsnop\Mysqldump as IMysqldump;
+use Mail, Fungsi;
 class BackupDbToEmail extends Command {
 
 	/**
@@ -37,31 +38,40 @@ class BackupDbToEmail extends Command {
 	 */
 	public function fire()
 	{
-		//
+		try {
+			$dbuser = env('DB_USERNAME');
+			$dbpassword = env('DB_PASSWORD');
+			$dbname = env('DB_DATABASE');
+			$dbhost = env('DB_HOST');			
+		    $dump = new IMysqldump\Mysqldump($dbname, $dbuser, $dbpassword, $dbhost, 'mysql', ['compress' => 'Gzip']);
+		    $path = storage_path('framework/cache/backup_db_'.date('Y-m-d').'.sql.gz');
+		    $dump->start($path);
+		    $this->info('Sukses Backup DB!');
+		} catch (\Exception $e) {
+		    $this->error('Import failed with message : ' . $e->getMessage());
+		}
+
+
+
+ 		$this->info('sending files to your email backup...');
+
+			Mail::send('emails.backup_database', array('key' => 'value'), function($message){
+ 			    $message->to(env('EMAIL_PENGIRIM'))
+			    ->subject('Backup Database '.env('NAMA_APP').', '.Fungsi::date_to_tgl(date('Y-m-d')) );
+			    $message->attach(storage_path('framework/cache/backup_db_'.date('Y-m-d').'.sql.gz'));
+			});
+
+			$file_db1 = storage_path('framework/cache/backup_db_'.date('Y-m-d').'.sql.gz');
+			if(file_exists($file_db1)){
+				unlink($file_db1);
+			}
+ 
+			$this->info('done!');
+
+
+
+
 	}
 
-	/**
-	 * Get the console command arguments.
-	 *
-	 * @return array
-	 */
-	protected function getArguments()
-	{
-		return [
-			['example', InputArgument::REQUIRED, 'An example argument.'],
-		];
-	}
-
-	/**
-	 * Get the console command options.
-	 *
-	 * @return array
-	 */
-	protected function getOptions()
-	{
-		return [
-			['example', null, InputOption::VALUE_OPTIONAL, 'An example option.', null],
-		];
-	}
-
+ 
 }
