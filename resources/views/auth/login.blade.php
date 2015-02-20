1ad48e463664d3489fa630c3b04170f1aeb3c29d
reka prihatanto <rey.barrolz@gmail.com>
2015-02-20 13:23:53 +0700

    rubah posisi layout halaman login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,23 +3,6 @@
 @section('main_konten')
 
 
-    <nav class="navbar navbar-fixed-top navbar-default">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="{!! URL::route('home.index') !!}"><i class='fa fa-cubes'></i> {!! env("NAMA_APP") !!} </a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-
-
-        </div><!-- /.nav-collapse -->
-      </div><!-- /.container -->
-    </nav><!-- /.navbar -->
 
 
   
@@ -109,9 +92,7 @@
 									<input type="checkbox" name="remember"> Remember Me
 								</label>
 							</div>
-							<div class="col-md-6">
-								<a href="/password/email">Forgot Your Password?</a>
-							</div>
+ 
 						</div>
 					</form>
 				</div>
--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -13,7 +13,7 @@
 
  
 
-
+@include('layouts.komponen.public.navbar')
 
 
 
