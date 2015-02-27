376d2c0196edecf34adf02606d32776e988238a7
reka prihatanto <rey.barrolz@gmail.com>
2015-02-27 14:13:07 +0700

    tambah halaman daftar berita

--- resources/views/konten/frontend/auth/login/list_file.blade.php
+++ resources/views/konten/frontend/auth/login/list_file.blade.php
@@ -1,13 +1,14 @@
-<div class='col-md-12 panel panel-default'>
-	<h3 style='padding-left:0.5em;border-left: solid 4px #ccc;'>File Terbaru</h3>
-	<hr style='margin:2px;'>
-	<ul>
-		@foreach($lampiran_berita as $list)
-		<?php
-		$id = $hashids->encode(1000, 2000, $list->id);
-		?>
-			<li> <a href="{!! URL::route('berita.download_lampiran', $id) !!}">{!! $list->nama !!}</a> </li>
-		@endforeach
-	</ul>
+ 
+	<div class='col-md-12 panel panel-default'>
+		<h3 style='padding-left:0.5em;border-left: solid 4px #ccc;'>File Terbaru</h3>
+		<hr style='margin:2px;'>
+		<ul>
+			@foreach($lampiran_berita as $list)
+			<?php
+			$id = $hashids->encode(1000, 2000, $list->id);
+			?>
+				<li> <a href="{!! URL::route('berita.download_lampiran', $id) !!}">{!! $list->nama !!}</a> </li>
+			@endforeach
+		</ul>
 
-</div>
+	</div>
---
+++ resources/views/konten/frontend/berita/daftar_berita/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.public')
+
+@section('main_konten')
+
+ 
+    <a class='btn btn-primary pull-right' href="{!! URL::route('berita.public_berita') !!}">beranda</a>
+
+      <h3 style='margin-top: 0px;font-weight:bold;'> <i class='fa fa-angle-right'></i> Daftar Berita</h3>
+      <hr style='margin:2px;'>
+
+ dad asdas
+
+
+
+
+@endsection
+
+
+ 
