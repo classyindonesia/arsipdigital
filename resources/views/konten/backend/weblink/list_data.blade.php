280fe232c5b67c5ecdfb2f92b95ee872cc52e6a3
reka prihatanto <rey.barrolz@gmail.com>
2015-03-03 14:42:19 +0700

    tambah file migration weblink

--- resources/views/konten/backend/weblink/list_data.blade.php
+++ resources/views/konten/backend/weblink/list_data.blade.php
@@ -1,11 +1,19 @@
 <table class="table table-bordered table-hover">
 	<thead>
 		<tr>
-			<th></th>
+			<th class='text-center' width='5%'>No</th>
+			<th>Nama</th>
+			<th>Kategori link</th>
+			<th>URL</th>
+			<th class='text-center' width='5%'>Action</th>
 		</tr>
 	</thead>
 	<tbody>
 		<tr>
+			<td class='text-center'></td>
+			<td></td>
+			<td></td>
+			<td></td>
 			<td></td>
 		</tr>
 	</tbody>
