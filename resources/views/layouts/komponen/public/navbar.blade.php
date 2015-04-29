f55d89b859e4fb79d46fb8b399f66a6fef37b9d1
reka prihatanto <rey.barrolz@gmail.com>
2015-04-29 08:50:40 +0700

    edit icon

--- resources/views/layouts/komponen/public/navbar.blade.php
+++ resources/views/layouts/komponen/public/navbar.blade.php
@@ -7,7 +7,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="{!! URL::route('home.index') !!}"><i class='{!! ICON_DEPAN !!}'></i> {!! env("NAMA_APP") !!} </a>
+          <a class="navbar-brand" href="{!! URL::route('home.index') !!}"><i class='{!! env("ICON_DEPAN") !!}'></i> {!! env("NAMA_APP") !!} </a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
 
