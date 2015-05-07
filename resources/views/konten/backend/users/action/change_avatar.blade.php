3b1cefcf8684d7acc6eb05fcc1f2ed54c7a4ca53
reka prihatanto <rey.barrolz@gmail.com>
2015-05-07 07:43:57 +0700

    perbaikan edit foto level admin

--- resources/views/konten/backend/users/action/change_avatar.blade.php
+++ resources/views/konten/backend/users/action/change_avatar.blade.php
@@ -1,10 +1,12 @@
-<i data-toggle='tooltip' title='ganti foto' class='fa fa-image' id='ganti_avatar{{ $list->id }}' style='cursor:pointer;'></i>
-<script type="text/javascript">
-$('#ganti_avatar{{ $list->id }}').click(function(){
-	$('.modal-body').html('loading... <i class="fa fa-spinner fa-spin"></i>');
-	$('#myModal').modal('show');
-	$('.modal-body').load('{{ URL::route("users.change_avatar", $list->id) }}')
+@if(count($list->mst_user)>0)
+	<i data-toggle='tooltip' title='ganti foto' class='fa fa-image' id='ganti_avatar{{ $list->mst_user->id }}' style='cursor:pointer;'></i>
+	<script type="text/javascript">
+	$('#ganti_avatar{{ $list->mst_user->id }}').click(function(){
+		$('.modal-body').html('loading... <i class="fa fa-spinner fa-spin"></i>');
+		$('#myModal').modal('show');
+		$('.modal-body').load('{{ URL::route("users.change_avatar", $list->mst_user->id) }}')
 
-})
-</script>
+	})
+	</script>
 
+@endif
