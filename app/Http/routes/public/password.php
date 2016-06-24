aeedf679aa78f456707737ca23e725ba42b15998
reka prihatanto <rey.barrolz@gmail.com>
2016-06-24 07:48:59 +0700

    edit route password

--- app/Http/routes/public/password.php
+++ app/Http/routes/public/password.php
@@ -1,6 +1,23 @@
 <?php
 
-	Route::controllers([
-		'password' => 'Auth\ResetPasswordsController',
-	]);	
+	// Route::controllers([
+	// 	'password' => 'Auth\ResetPasswordsController',
+	// ]);	
  
+Route::get('password/email/{one?}/{two?}/{three?}/{four?}/{five?}',[
+	'uses'	=> 'Auth\ResetPasswordsController@getEmail'
+]);
+
+Route::post('password/email/{one?}/{two?}/{three?}/{four?}/{five?}',[
+	'uses'	=> 'Auth\ResetPasswordsController@postEmail'
+]);
+
+
+Route::get('password/reset/{one?}/{two?}/{three?}/{four?}/{five?}',[
+	'uses'	=> 'Auth\ResetPasswordsController@getReset'
+]);
+
+Route::post('password/reset/{one?}/{two?}/{three?}/{four?}/{five?}',[
+	'uses'	=> 'Auth\ResetPasswordsController@postReset'
+]);
+
